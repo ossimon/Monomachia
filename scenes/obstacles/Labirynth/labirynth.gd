49f442cfa9dba37f6853487bd31d11024d34cdf7
fkitka <filip5845@gmail.com>
2024-04-30 17:29:45 +0200

    Add generated blocks for labirynth

--- scenes/obstacles/Labirynth/labirynth.gd
+++ scenes/obstacles/Labirynth/labirynth.gd
@@ -1,64 +1,92 @@
 extends Node2D
 
+const Block = preload("res://scenes/obstacles/Block/block.tscn")
+const PlayerWall = preload("res://scenes/obstacles/Block/player_wall.tscn")
 
-var player_wall: PackedScene = preload("res://scenes/obstacles/Block/player_wall.tscn")
+const WALL_SIZE = Vector2(20, 20)
+const OFFSET = 10
 
-var wall_size = Vector2(20, 20)
-var offset = 10
+const GRID_WIDTH = 20
+const GRID_HEIGHT = 15
 
-var grid_width = 20
-var grid_height = 15
-# 2D array to store grid state
 var grid = []
 
 func _ready():
-	for x in range(grid_width):
+	grid = generate_grid(GRID_WIDTH, GRID_HEIGHT)
+	#print_grid()
+
+func generate_grid(width, height):
+	var grid = []
+	for i in range(height):
 		grid.append([])
-		for y in range(grid_height):
-			grid[x].append(false)
+		for j in range(width):
+			if randf() < 0.2:
+				grid[i].append(true)
+				add_block_at_position(position_from_grid_index(j, i))
+			else:
+				grid[i].append(false)
+	return grid
+
+func print_grid():
+	for row in grid:
+		print(row)
 
 func position_to_grid_index(pos):
-	var x_index = int((round(pos.x) + 200) / wall_size.x)
-	var y_index = int((round(pos.y) + 150) / wall_size.y)
+	var x_index = int((round(pos.x) + 200) / WALL_SIZE.x)
+	var y_index = int((round(pos.y) + 150) / WALL_SIZE.y)
+	#print("Position: ", pos.x, " ", pos.y, " to ", "Index: ",x_index, " ", y_index)
 	return [x_index, y_index]
-
+	
+func position_from_grid_index(x, y):
+	return Vector2(
+		x * WALL_SIZE.x - (200 - OFFSET),
+		y * WALL_SIZE.y - (150 - OFFSET)
+	)
+	
 func add_node_at_position(pos):
-	if (pos.x <= -200 || pos.x >= 200) || (pos.y <= -150 || pos.y >= 150):
+	if is_out_of_bounds(pos):
 		print("Building out of bounds")
 		return
 
-	pos = position_to_grid_index(pos)
-	var x = pos[0]
-	var y = pos[1]
+	var grid_index = position_to_grid_index(pos)
+	var x = grid_index[0]
+	var y = grid_index[1]
 	
-	if (!is_node_at_position(x, y)):
-		var wall = player_wall.instantiate()
-		wall.connect("is_shattered", _on_player_wall_is_shattered)
-		var center_position = Vector2(
-			(x * wall_size.x) - (200 - offset), 
-			(y * wall_size.y) - (150 - offset)
-		)
-		wall.position = center_position
-		add_child(wall)
-		grid[x][y] = true
+	if !is_node_at_position(x, y):
+		var node = PlayerWall.instantiate()
+		node.connect("is_shattered", _on_player_wall_is_shattered)
+		
+		node.position = position_from_grid_index(x, y)
+		add_child(node)
+		grid[y][x] = true
 	else:
-		print("Cant build a wall")
+		print("Can't build a wall")
 
 func remove_node_at_position(pos):
-	pos = position_to_grid_index(pos)
-	var x = pos[0]
-	var y = pos[1]
+	var grid_index = position_to_grid_index(pos)
+	var x = grid_index[0]
+	var y = grid_index[1]
 	
-	if (is_node_at_position(x, y)):
-		grid[x][y] = false
+	if is_node_at_position(x, y):
+		grid[y][x] = false
 	else:
-		print("No wall")
-	pass
+		print("No need to remove")
 
 func is_node_at_position(x, y):
-	if (x > 0 && x <= grid_width) && (y > 0 && y <= grid_height):
-		return grid[x][y]
+	if (x > 0 && x <= GRID_WIDTH) && (y > 0 && y <= GRID_HEIGHT):
+		return grid[y][x]
 
+func is_out_of_bounds(pos):
+	return (pos.x <= -200 || pos.x >= 200) || (pos.y <= -150 || pos.y >= 150)
+
+func add_block_at_position(pos):
+	var block_instance = Block.instantiate()
+	block_instance.connect("is_shattered", _on_block_is_shattered)
+	block_instance.position = pos
+	add_child(block_instance)
 
 func _on_player_wall_is_shattered(pos):
+	remove_node_at_position(pos)
+	
+func _on_block_is_shattered(pos):
 	remove_node_at_position(pos)
