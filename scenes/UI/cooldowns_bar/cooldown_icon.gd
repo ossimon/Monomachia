f326da35ee6564ec44faae829541e8c9e7ddba5d
unknown <oskarsimon3@gmail.com>
2024-06-10 17:55:06 +0200

    CD labels

--- scenes/UI/cooldowns_bar/cooldown_icon.gd
+++ scenes/UI/cooldowns_bar/cooldown_icon.gd
@@ -3,7 +3,10 @@ extends Node2D
 func update(timer: Timer):
 	if timer.is_stopped():
 		$TextureProgressBar.value = 0
+		$Label.text = ''
 		return
 	
 	$TextureProgressBar.value = timer.time_left * 100 / timer.wait_time
+	# show one decimal place
+	$Label.text = str(timer.time_left).left(3)
 
